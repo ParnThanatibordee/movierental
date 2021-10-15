da173e2df533af349a58cfca94b377aea00481bc
ParnThanatibordee <parn8946@gmail.com>
2021-10-16 02:37:37 +0700

    Removing Temps

--- customer.py
+++ customer.py
@@ -29,27 +29,37 @@ class Customer:
             Returns:
                 the statement as a String
         """
-        total_amount = 0  # total charges
-        frequent_renter_points = 0 
         statement = f"Rental Report for {self.name}\n\n"
         fmt = "{:32s}    {:4s} {:6s}\n"
         statement += fmt.format("Movie Title", "Days", "Price")
         fmt = "{:32s}   {:4d} {:6.2f}\n"
         
         for rental in self.rentals:
-            frequent_renter_points += rental.get_frequent()
             #  add detail line to statement
             statement += fmt.format(rental.get_movie().get_title(), rental.get_days_rented(), rental.get_price())
             # and accumulate activity
-            total_amount += rental.get_price()
 
         # footer: summary of charges
         statement += "\n"
         statement += "{:32s} {:6s} {:6.2f}\n".format(
-                       "Total Charges", "", total_amount)
-        statement += "Frequent Renter Points earned: {}\n".format(frequent_renter_points)
+                       "Total Charges", "", self.get_total_amount())
+        statement += "Frequent Renter Points earned: {}\n".format(self.get_frequent_renter_points())
 
         return statement
+
+    def get_total_amount(self):
+        result = 0
+        for rental in self.rentals:
+            result += rental.get_price()
+
+        return result
+
+    def get_frequent_renter_points(self):
+        result = 0
+        for rental in self.rentals:
+            result += rental.get_frequent()
+
+        return result
 
 
 if __name__ == "__main__":
